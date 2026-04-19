3d98bf804115b02ba469d35ef252f1d39039215a
@EDDY.GONZALEZQUIROS <eddy.gonzalezquiros@ucr.ac.cr>
2026-04-19 16:08:29 -0600

    Se hicieron cambios en App.vue

---
+++ PortafolioMultimedia/src/components/MiPrimerComponente.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="box">
+    <h2>Mi primer componente en Vue 3</h2>
+    <p>Proyecto IF7102 – 2026</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.box {
+  padding: 1rem;
+  border: 1px solid #ccc;
+}
+</style>
